e34b6656d518473e6a24715c64f0a1e371bc633c
TheGreatHumble <36174488+TheGreatHumble@users.noreply.github.com>
2018-04-11 15:33:49 -0500

    Add files via upload

---
+++ myls.c
@@ -0,0 +1,34 @@
+//
+//  myls.c
+//  MyFirstShell
+//
+//  Created by LeanderDavis on 4/10/18.
+//  Copyright (c) 2018 LeanderDavis. All rights reserved.
+//
+
+#include "myls.h"
+//#include "apue.h"
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    DIR *dp;
+    struct dirent *dirp;
+    
+    if (argc != 2) {
+        //err_quit("usage: ls directory_name");
+        fprintf(stderr, "usage: myls requires directory name");
+        exit(0) ;
+    }
+    if ((dp = opendir(argv[1]))==NULL) {
+        //err_sys("can't open %s", argv[1]);
+        fprintf(stderr, "%s can't be opened", argv[1]);
+        exit(0);
+    }
+    while ((dirp = readdir(dp)) != NULL) {
+        printf("%s\n", dirp->d_name);
+    }
+    closedir(dp);
+    exit(0);
+}
