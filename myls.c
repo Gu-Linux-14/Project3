521945463e490df4e30a4f7bd4a8acd46cd3afe5
Elijah Garcia <e_g257@txstate.edu>
2018-04-24 22:02:59 +0000

    Update myls.c

--- myls.c
+++ myls.c
@@ -1,13 +1,4 @@
-//
-//  myls.c
-//  MyFirstShell
-//
-//  Created by LeanderDavis on 4/10/18.
-//  Copyright (c) 2018 LeanderDavis. All rights reserved.
-//
-
 #include "myls.h"
-//#include "apue.h"
 #include <dirent.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,52 +6,47 @@
 #include <sys/types.h>
 
 int main(int argc, char *argv[]){
-    
-}
-
-int mylsNoOp(int argc, char *argv[]){
-    DIR *dp;
-    struct dirent *dirp;
-    
-    if (argc != 2) {
-        //err_quit("usage: ls directory_name");
-        fprintf(stderr, "usage: myls requires directory name");
-        exit(0) ;
-    }
-    if ((dp = opendir(argv[1]))==NULL) {
-        //err_sys("can't open %s", argv[1]);
-        fprintf(stderr, "%s can't be opened", argv[1]);
-        exit(0);
-    }
-    while ((dirp = readdir(dp)) != NULL) {
-        printf("%s\n", dirp->d_name);
-    }
-    closedir(dp);
-    exit(0);
-}
-
-int mylsL(int argc, char *argv[]){
     DIR *dp;
     struct dirent *dirp;
     struct stat mystat;
     char buf[512];
     
-    if (argc != 2) {
+    if(argc < 2){
         //err_quit("usage: ls directory_name");
         fprintf(stderr, "usage: myls requires directory name");
         exit(0) ;
     }
-    if ((dp = opendir(argv[1]))==NULL) {
-        //err_sys("can't open %s", argv[1]);
-        fprintf(stderr, "%s can't be opened", argv[1]);
-        exit(0);
+
+    if(argv[2] == NULL){
+        if((dp = opendir(argv[1]))==NULL){
+            fprintf(stderr, "%s can't be opened\n", argv[1]);
+            exit(0);
+        }
     }
-    while ((dirp = readdir(dp)) != NULL) {
-        printf("%s\n", dirp->d_name);
-        stat(buf, &mystat);
-        printf("%zu", mystat.st_size);
-        printf(" %s\n", dirp->d_name);
+
+    if(argv[2] != NULL){
+        if((dp = opendir(argv[2]))==NULL){
+            fprintf(stderr, "%s can't be opened\n", argv[1]);
+            exit(0);
+        }
     }
+
+    printf("%d\n", argc);
+    if(argc == 2){
+        while((dirp = readdir(dp)) != NULL){
+            printf("%s\n", dirp->d_name);
+        }
+    }
+    else if(argc == 3){
+        while((dirp = readdir(dp)) != NULL){
+            printf("%s\n", dirp->d_name);
+            stat(buf, &mystat);
+            printf("%zu", mystat.st_size);
+            printf(" %s", dirp->d_name);
+        }
+        printf("\n");
+    }
+    
     closedir(dp);
-    exit(0);
+    exit(0); 
 }
