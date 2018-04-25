40bbb7cf0662d7a7ed4c22b714a7186576e5ba2a
lms251 <31562399+lms251@users.noreply.github.com>
2018-04-25 16:40:04 -0500

    -R doesn't work

--- mycp.c
+++ mycp.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
+#include <dirent.h>
+#include <string.h>
 
 int mycp(int argc, char**argv[]) {
 	if(argc == 3){
@@ -22,7 +23,7 @@ int mycp(int argc, char**argv[]) {
 int mycpFile(char *file1, char *file2) {
 	FILE *fip;// = fopen(file1, "r");
 	if(!(fip = fopen(file1, "r"))){
-		printf("failed to openfile\n");
+		printf("failed to openfile %s\n", file1);
 		return -1;
 	}
 	FILE *fop = fopen(file2, "w+");
@@ -34,6 +35,23 @@ int mycpFile(char *file1, char *file2) {
 }
 
 int mycpRecurr(char* dir1, char* dir2) {
+	DIR *dir;
+	struct dirent *dirent;
+	
+	if ((dir = opendir( dir1 )) == NULL) {
+		printf("Cannot open directory %s\n", dir1);	
+	}
+	else {
+		while ((dirent = readdir(dir)) != NULL) {
+			if(dirent->d_name == "./" || dirent->d_name == "../") {
+				continue;
+			}
+			//printf("%s\n", dirent->d_name);	
+			while((dirent = readdir(dir)) != NULL) {				
+				mycpFile(dirent->d_name, "../dir2/f1");			
+			}
+		}	
+	}
 	return 0;
 }
 
